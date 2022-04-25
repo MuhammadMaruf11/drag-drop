b3a3279be8430f8a80e3b6b5e1a6fd3ce7555bf8
Muhammad Maruf <88553825+MuhammadMaruf11@users.noreply.github.com>
2022-04-25 17:41:58 +0600

    2nd commit

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const dragArea = document.querySelector('.appBody'),
     browseBtn = dragArea.querySelector('button'),
     inputBtn = dragArea.querySelector('input');
 
+const showImg = document.querySelector('.icon');
+
 let myFile;
 
 browseBtn.onclick = () => {
     inputBtn.click()
 }
 
-inputBtn.addEventListener('change', function () {
+inputBtn.addEventListener('change', () => {
     myFile = this.files[0];
     dragArea.classList.add('active');
 
@@ -30,6 +32,7 @@ dragArea.addEventListener('dragleave', () => {
 dragArea.addEventListener('drop', (e) => {
     e.preventDefault();
     myFile = e.dataTransfer.files[0];
+    console.log(myFile);
 
     ShowMe()
 })
@@ -40,16 +43,18 @@ function ShowMe() {
 
     if (vaildEx.includes(fileType)) {
         let fileReader = new FileReader();
+        console.log(fileReader);
         fileReader.onload = () => {
             let imgUrl = fileReader.result;
             let img = `<img src="${imgUrl}" alt=""> `;
 
-            dragArea.innerHTML = img;
+            showImg.innerHTML = img;
         }
         fileReader.readAsDataURL(myFile);
-    } else{
-        alert('This file is not valid')
+    } else {
+        alert('This file is not valid, please upload a valid image')
         dragArea.classList.remove('active');
         dragText.textContent = 'Drag & Drop';
+        showImg.innerHTML = `<img src="img.jpg" alt=""> `;
     }
 }
